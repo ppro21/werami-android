a35831472c279a75e4a318afa86fb34ef300daf6
ppro21 <paolo.prodev@gmail.com>
2021-09-27 08:42:48 +1300

    Create package fragments and transfer fragment-files.

--- app/src/main/java/com/pprodev/werami/fragments/HomeFragment.kt
+++ app/src/main/java/com/pprodev/werami/fragments/HomeFragment.kt
@@ -1,20 +1,29 @@
-package com.pprodev.werami
+package com.pprodev.werami.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.pprodev.werami.R
 
 
-class MapFragment : Fragment() {
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
 }
